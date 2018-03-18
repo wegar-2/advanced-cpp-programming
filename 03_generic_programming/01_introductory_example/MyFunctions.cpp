7151df02aad3794aa77fba9f86b4f4600213c70b
wegar-2 <prometheus0209@gmail.com>
2018-03-18 10:20:37 +0100

    Adding a description of template function instantiation and having a tough time getting to understand that when gitignore file starts ignoring a pattern it does not immediately ignore files that are already being tracked. Apparently reading docs does not hurt.

--- 03_generic_programming/01_introductory_example/MyFunctions.cpp
+++ 03_generic_programming/01_introductory_example/MyFunctions.cpp
@@ -20,3 +20,14 @@ void MyAnotherTemplateFunction(T1 arg1, T2 arg2) {
 // explicit instantiation
 template int MyTemplateFunction<int> (int arg1);
 template void MyAnotherTemplateFunction<int, double> (int arg1, double arg2);
+
+/*
+Explicit instantiation of template functions is performed in the way demonstrated above.
+1) all the types placeholders have to be indicated in chevrons immediately after the
+    function name
+2) if the template function does have placeholders used in the definition of its
+    parameters, these have to be explicitly indicated when instantiating the
+    template function
+3) similarly, if the template function's returned type is a placeholder, it has
+    to be explicitly provided when instantiating the function template
+*/
