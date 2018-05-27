0279f8b1f33133aae5a9fabe10219fec9b765c95
wegar-2 <prometheus0209@gmail.com>
2018-05-27 22:44:24 +0200

    Added code on STL lists - implementation of double-linked list.

--- 06_sequential_containers/04_list_class_part2/main.cpp
+++ 06_sequential_containers/04_list_class_part2/main.cpp
@@ -10,26 +10,34 @@ int main()
     /* ************************************************************** */
     /* *********** revision of the material on list class *********** */
     /* ************************************************************** */
-    // 1.
-    list<int> ListOfInts(100);
-    for (unsigned int k = 0; k < 100; k++) {
+    // 1. create a list of integers
+    list<int> ListOfInts(0);
+    for (unsigned int k = 0; k < 10; k++) {
         ListOfInts.push_back((rand() % 100) + 1);
     }
     cout << "ListOfInts.front() = " << ListOfInts.front() << endl;
     cout << "ListOfInts.back() = " << ListOfInts.back() << endl;
-    // 2.
+    // 2. print the list of integers using an iterator
+    cout << "Printing the list of integers: " << endl;
     list<int>::iterator iter1 = ListOfInts.begin();
     cout << "Displaying list ListOfInts: " << endl;
     while (iter1 != ListOfInts.end()) {
         cout << "\t" << *iter1 << endl;
         iter1++;
     }
-    // 3.
+    // 3. print the list of integers in reverse order
+    cout << "Printing the list of integers in reverse order: " << endl;
     ListOfInts.reverse();
-    list<int>::iterator iter2 =
-
-    // 4.
-
-
+    list<int>::iterator iter2 = ListOfInts.begin();
+    for (iter2; iter2 != ListOfInts.end(); iter2++) {
+        cout << "\t" << *iter2 << endl;
+    }
+    // 4. sort the list of integers and print it
+    cout << "Printing the sorted list of integers: " << endl;
+    ListOfInts.sort();
+    list<int>::iterator iter3 = ListOfInts.begin();
+    for (iter3; iter3 != ListOfInts.end(); iter3++) {
+        cout << "\t" << *iter3 << endl;
+    }
     return 0;
 }
