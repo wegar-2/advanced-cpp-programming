5919c1d169b50b4d16e50d6e05d1570d2b0bbbad
wegar-2 <prometheus0209@gmail.com>
2018-09-22 15:49:44 +0200

    A snippet on templates.

--- _using_boost/3_templates_revision_part2/main.cpp
+++ _using_boost/3_templates_revision_part2/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include <string>
-#include "TempClass1.hpp"
+#include <vector>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
 
+using std::cout;
+using std::endl;
 using std::string;
+using std::vector;
 
 int main()
 {
-    TempClass1<int, string> obj1 = TempClass1<int, string>(10, string("qwerty"));
+    // vector in the stack
+    boost::random::mt19937 mt_gtr = boost::random::mt19937();
+    boost::random::uniform_int_distribution<> my_distrib =
+        boost::random::uniform_int_distribution<>();
+    vector<double> vec1(10);
+    for (int k=0; k<10; ++k) {
+        vec1[k] = my_distrib(mt_gtr);
+    }
+
+    // vector in the heap
+    vector<int>* pVec = new vector<int>(10);
+    for (int k=0; k<10; ++k) {
+        (*pVec)[k] = 100;
+    }
+    for (int k=0; k<10; ++k) {
+        cout << "heap vector's element nr " << k << ": "
+        << (*pVec)[k] << endl;
+    }
+
     return 0;
 }
